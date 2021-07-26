747b2832f4a13a42da743e121126e2cfe5e022b4
Mikey <mikeywearing@gmail.com>
2021-07-26 16:14:15 +0200

    Started to work on SideBar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,12 +1,34 @@
 import React from "react";
 
 // Styles
-import { SideBarContainer } from "./SideBar.styles";
+import {
+  SideBarContainer,
+  Icon,
+  CloseIcon,
+  SideBarWrapper,
+  SideBarLink,
+} from "./SideBar.styles";
 
 export default function SideBar({ isOpen, toggle }) {
   return (
-    <SideBarContainer isOpen={isOpen} onClick={toggle} >
-        Hey
+    <SideBarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SideBarWrapper>
+        <SideBarLink to="about" smooth={true} offset={-50} onClick={toggle}>
+          About
+        </SideBarLink>
+        <SideBarLink to="skills" smooth={true} offset={-50} onClick={toggle}>
+          Skills
+        </SideBarLink>
+        <SideBarLink to="projects" smooth={true} offset={-50} onClick={toggle}>
+          Projects
+        </SideBarLink>
+        <SideBarLink to="contact" smooth={true} offset={100} onClick={toggle}>
+          Contact
+        </SideBarLink>
+      </SideBarWrapper>
     </SideBarContainer>
-  )
+  );
 }
--- src/components/SideBar/SideBar.styles.js
+++ src/components/SideBar/SideBar.styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Link } from "react-scroll";
+import { FaTimes } from "react-icons/fa";
 
 export const SideBarContainer = styled.aside`
   background: deepskyblue;
@@ -14,4 +16,41 @@ export const SideBarContainer = styled.aside`
   transition: 0.3s ease-in-out;
   opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
   top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+`;
+
+export const CloseIcon = styled(FaTimes)`
+	color: black;
+`;
+
+export const Icon = styled.div`
+	position: absolute;
+	top: 1.2rem;
+	right: 1.5rem;
+	background: transparent;
+	font-size: 2rem;
+	cursor: pointer;
+	outline: none;
+`;
+
+export const SideBarWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, 80px);
+  text-align: center;
+`;
+
+export const SideBarLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 5vh;
+  /* text-decoration: none;
+  list-style: none; */
+  transition: 0.2s ease-in-out;
+  color: black;
+
+  &:hover {
+    color: white;
+    transition: 0.2s ease-in-out;
+  }
 `;
